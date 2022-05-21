0435463de7bb538ccdc741b51be2db5f07e0817f
babasow <babasow120@gmail.com>
2022-05-21 16:10:30 +0200

    Edit done

--- routes/articles.js
+++ routes/articles.js
@@ -1,20 +1,29 @@
 const express = require("express");
-const article = require("./../models/article");
 const Article = require('./../models/article')
 const router = express.Router();
+const mongoose = require('mongoose');
 
 router.get('/newArticle', (req, res) => {
   res.render('newArticle', {article : new Article()})
 });
 
 router.get("/:id", async(req, res)=>{
-  const article = await Article.findOne({id: req.params.id});
+  const article = await Article.findOne({_id: mongoose.Types.ObjectId(req.params.id)});
+  console.log(req.params.id);
   if(article == null) {
     res.redirect('/');
   }
+  console.log(article)
   res.render("show" , {article : article})
 });
 
+router.get("/edit/:id", async(req, res)=>{
+let article = await Article.findOne({_id: mongoose.Types.ObjectId(req.params.id)});
+  if(article == null) {
+    res.redirect('/');
+  }
+  res.render("newArticle" , {article : article})
+});
 
 router.post('/', async(req, res) => {
   let article = new Article({
@@ -32,8 +41,16 @@ router.post('/', async(req, res) => {
 });
 
 router.delete("/:id", async(req, res)=>{
-  await Article.findByIdAndDelete(req.params.id);
+  await Article.findByIdAndDelete(mongoose.Types.ObjectId(req.params.id));
    res.redirect('/');
 });
 
+
+router.put('/edit/:id', async(req, res)=> {
+    let article = Article.findById(mongoose.Types.ObjectId(req.params.id));
+    if (article == null){
+      res.redirect('/')
+    }
+    Article.updateOne(req.body);
+})
 module.exports = router ;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const res = require('express/lib/response');
 const mongoose = require('mongoose');
-const article = require('./models/article')
+const Article = require('./models/article')
 const router = require('./routes/articles')
 const override = require('method-override');
 const methodOverride = require('method-override');
@@ -13,7 +13,7 @@ app.set('view engine' , 'ejs');
 app.use(express.urlencoded({extended: false}));
 app.use(methodOverride('_method'))
 app.get('/' ,async(req, res) => {
-  let articles = await article.find();
+  let articles = await Article.find();
    res.render('index' , {articles : articles });
 });
 
